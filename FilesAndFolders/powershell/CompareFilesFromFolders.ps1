fd79134bc8b61fec5abbd3a60a44544987c4bfe2
Christian Popescu <christian.popescu@outlook.com>
2021-05-27 22:56:52 +0200

    Find max lengh path

--- FilesAndFolders/powershell/CompareFilesFromFolders.ps1
+++ FilesAndFolders/powershell/CompareFilesFromFolders.ps1
@@ -1,5 +1,12 @@
 # Compare Files From Folders
 
-$pathFirst = "F:\temp\"
+$pathFirst = "F:\CCP_library\Doc_IT_New"
+$max = 0
+$fullPath = ""
 
-get-childitem -recurse -File -Path $pathFirst 
+get-childitem -recurse -File -Path $pathFirst | 
+ForEach-Object -Process {if ($_.FullName.Length -gt $max) 
+{ $max = $_.FullName.Length
+$fullPath = $_.FullName } }
+write-host $max
+write-host $fullPath
